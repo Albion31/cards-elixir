8cbc303c1cede54c0753eadf5c79c4e515226f7e
Lan <lanpham312@gmail.com>
2018-06-04 13:45:59 +0100

    refactoring comprehensive

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,16 +3,10 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    cards = for value <- values do
-      for suit <- suits do
+    for suit <- suits, value <- values do
         "#{value} of #{suit}"
-      end
     end
-
-    List.flatten(cards)
   end
-
-
 
   def shuffle(deck) do
     Enum.shuffle(deck)
